752e43f5354002996af74f920db6a974bcf88fdd
Satit Riapit <siteslave@msn.com>
2019-05-13 11:40:39 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface ICar {
+  brand: string;
+  model?: string;
+  type?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leave-system';
+
+  brand: string;
+  model: string;
+  type: string;
+
+  types = ['SPORT', 'MINI', 'VAN'];
+
+  fruits = ['Apple', 'Banana', 'Orange'];
+  cars = [
+    {brand: 'TOYOTA', model: 'Revo'},
+    {brand: 'HONDA', model: 'Civic'}
+  ];
+
+  addCar() {
+    const obj: ICar = {
+      brand: 'xx',
+      model: 'xxx',
+      type: this.type
+    };
+
+    const obj2: any = {};
+    obj2.model = this.model;
+    obj2.brand = this.brand;
+    obj2.type = this.type;
+
+    this.cars.push(obj2);
+  }
+
+  removeCar(idx: number) {
+    this.cars.splice(idx, 1);
+  }
+
 }
